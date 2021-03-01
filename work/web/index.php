0a0057ea6561e8168ba78268bccf92af2f12642b
花本敬太 <hanamotokeita@hanamottanoiMac.sensyu.ne.jp>
2021-03-01 22:47:16 +0900

    2回目

---
+++ work/web/index.php
@@ -0,0 +1,28 @@
+<?php
+require('../app/functions.php');
+
+$names = [
+  // 'Taro',
+  // 'Jiro',
+  // 'Saburo',
+]
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Practice</title>
+</head>
+<body>
+  <ul>
+    <?php if (empty($names)) {?>
+      <li>Nobody!</li>
+    <?php } else {?>
+      <?php foreach ($names as $name) {?>
+        <li><?= h($name); ?></li>
+      <?php }?>
+    <?php } ?>
+  </ul>
+</body>
+</html>
